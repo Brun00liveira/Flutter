5a8b1d1c1cb5740070f1fabce815933415cd41dd
Bruno <brunobromo321@gmail.com>
2024-06-23 01:22:16 -0300

    feat - rows

--- lib/home.dart
+++ lib/home.dart
@@ -32,6 +32,30 @@ class HomePageState extends State<HomePage> {
               style: TextStyle(fontSize: 30),
             ),
             CustomSwitch(),
+            Container(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.red,
+                )
+              ],
+            ),
           ],
         ),
       ),
