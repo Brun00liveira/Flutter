e0aaebb9bba1f1b586eee68a51ce86f106db8b0a
Bruno <brunobromo321@gmail.com>
2024-06-23 01:09:57 -0300

    feat - column flutter

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/app_controller.dart';
+import 'package:greengrocer/switch.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,14 +17,22 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: [CustomSwitch()],
         backgroundColor: Colors.blue, // Defina a cor desejada aqui
       ),
-      body: Center(
-        child: Switch(
-          value: AppController.instance.isDarkTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-          },
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Contador: $counter',
+              style: TextStyle(fontSize: 30),
+            ),
+            CustomSwitch(),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
