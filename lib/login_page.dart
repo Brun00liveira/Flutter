838d3c095749602bae794e2bd5b3916601d4e818
Bruno <brunobromo321@gmail.com>
2024-06-24 06:21:21 -0300

    image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,64 +12,104 @@ class LoginPage extends StatefulWidget {
 class LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image.asset('images/logo.png'),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image.asset('images/logo.png'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, right: 12),
+                child: Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                            left: 12, right: 12, top: 10, bottom: 10),
+                        child: Column(
+                          children: [
+                            TextField(
+                              onChanged: (text) {
+                                email = text;
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                  labelText: 'Email',
+                                  border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(height: 16),
+                            TextField(
+                              onChanged: (text) {
+                                password = text;
+                              },
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.green
+                                 
+                              ),
+                              onPressed: () {
+                                if (email == "brunobromo321@gmail.com" &&
+                                    password == "123") {
+                                  Navigator.of(context)
+                                      .pushReplacementNamed('/home');
+                                } else {
+                                  print('incorreto');
+                                }
+                              },
+                              child: const Text("Salvar"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == "brunobromo321@gmail.com" &&
-                        password == "123") {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('incorreto');
-                    }
-                  },
-                  child: const Text("Salvar"),
-                ),
-                const SizedBox(height: 16),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            'images/logo.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          color: Colors.black.withOpacity(0.5),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
